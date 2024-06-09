3cb853fef5fa06f9f737ed80c6f754408b0bff48
root <manuelantognazza@gmail.com>
2024-06-08 21:11:53 -0300

    endpoint amenities

--- API/amenities.py
+++ API/amenities.py
@@ -1,2 +1,87 @@
 #!/usr/bin/python3
-"""amenities endpoint"""
+"""amenities endpoint
+POST /amenities: Create a new amenity.
+GET /amenities: Retrieve a list of all amenities.
+GET /amenities/{amenity_id}: Retrieve detailed information about a specific amenity.
+PUT /amenities/{amenity_id}: Update an existing amenity’s information.
+DELETE /amenities/{amenity_id}: Delete a specific amenity."""
+
+from flask import Flask, jsonify, request
+
+app = Flask(__name__)
+
+
+@app.route('/amenities', methods=["POST"])
+def create_Amenities():
+    data = request.get_json()
+
+    if not data or not data['name']:
+        return jsonify({'error': "No date"}), 400
+
+    if not ValidNameAmenities(data):
+        return jsonify({'error': "amenities duplicated"}), 409
+
+    amenities = createAmenities(data)
+
+    if amenities:
+        return jsonify({"id": amenities['id'], "name": amenities['name'],
+                        "created_at": amenities['created_at'],
+                        "updated_at": amenities['updated_at']}), 201
+
+    return jsonify({'error': "error al crear"}), 400
+
+@app.route('/amenities')
+def ger_all_amenities():
+    amenities = getAllAmenities()
+
+    return jsonify([{"id": ameni['id'], "name": ameni['name'],
+                     "created_at": ameni['created_at'],
+                     "updated_at": ameni['updated_at']} for ameni in amenities]), 200
+
+@app.route('/amenities/<amenity_id>')
+def get_amenities(amenity_id):
+    amenities = getAmenities(amenity_id)
+
+    if not amenities:
+        return jsonify({'error': "amenitie not fund"}), 404
+
+    return jsonify({"id": amenities['id'], "name": amenities["name"],
+                    "created_at": amenities['created_at'],
+                    "updated_at": amenities['updated_at']}), 200
+
+
+@app.route('/amenities/<amenity_id>', methods=["PUT"])
+def update_Amenities(amenity_id):
+    data = request.get_json()
+
+    if not data or data['name']:
+        return jsonify({'error': "data Null"}), 400
+
+    amenity = getAmenities(amenity_id)
+
+    if not amenity:
+        return jsonify({'error': "no se encontro la amenity"}), 404
+
+    if getAmenitiesName(amenity['name']) and data['name'] != amenity['name']:
+        return jsonify({'error': "amenity not exists"}), 409
+
+    amenity = updateAmenities(amenity_id, data)
+
+    if amenity:
+        return jsonify({"id": amenity['id'], "name": amenity["name"],
+                        "created_at": amenity['created_at'],
+                        "updated_at": amenity['updated_at']}), 200
+    
+    return jsonify({'error': "no se pudo updatear"}), 400
+
+
+@app.route('/amenities/<amenity_id>', methods=["DELETE"])
+def delete_Amenities(amenity_id):
+    amenity = getAmenities(amenity_id)
+
+    if not amenity:
+        return jsonify({'error': "error al eliminar"}), 404
+
+    deleteAmenities(amenity_id)
+    
+    return jsonify({'message': "se borro la amenity"}), 204
--- API/cities.py
+++ API/cities.py
@@ -18,7 +18,10 @@ def get_Cities_For_Contr(country_code):
     cities = getCitiesFromContries(country_code)
 
     if cities:
-        return jsonify([{'id': city['id'], 'name': city['name'], 'country_code': city['country_code'], 'created_at': city['created_at'], 'updated_at': city['updated_at']} for city in cities]), 200
+        return jsonify([{'id': city['id'], 'name': city['name'],
+                         'country_code': city['country_code'],
+                         'created_at': city['created_at'],
+                         'updated_at': city['updated_at']} for city in cities]), 200
 
     return jsonify({'message': 'no hay elementos'}), 200
 
@@ -36,14 +39,19 @@ def cereate_Cities():
     if not name or not code:
         return jsonify({'error': "400 Bad Request"}), 400
 
-    if CodeValid(code):
+    if CodeValidCountry(code):
         return jsonify({'error': "el codigo no es valido"}), 400
 
     if CodeAndCityValid(code, name):
         return jsonify({'error': "nombre diplicado o invalido"}), 409
+    
+    city = cereateCities(data)
 
-    if cereateCities(data):
-        return jsonify({"id": data['id'], "name": data['name'], "country_code": data['country_code'], "created_at": data['country_code'], "updated_at": data['updated_at']}), 201
+    if city:
+        return jsonify({"id": city['id'], "name": city['name'],
+                        "country_code": city['country_code'],
+                        "created_at": city['country_code'],
+                        "updated_at": city['updated_at']}), 201
 
     return jsonify({'error': "error al crear la city"}), 400
 
@@ -53,6 +61,67 @@ def get_All_Cities():
     cities = getAllCities()
 
     if cities:
-        return jsonify([{"id": city['id'], "name": city['name'], "country_code": city['country_code'], "created_at": city['created_at'], "updated_at": city['updated_at']} for city in cities]), 200
+        return jsonify([{"id": city['id'], "name": city['name'],
+                         "country_code": city['country_code'],
+                         "created_at": city['created_at'],
+                         "updated_at": city['updated_at']} for city in cities]), 200
 
     return jsonify({'message': "no tiene cities"}), 200
+
+
+@app.route('/cities/<city_id>')
+def get_Cities(city_id):
+    city = getCitiesInId(city_id)
+
+    if not city:
+        return jsonify({'error': "404 Not Fund"}), 404
+
+    return jsonify({"id": city['id'], "name": city['name'],
+                    "country_code": city['country_code'],
+                    "created_at": city['created_at'],
+                    "updated_at": city['updated_at']}), 200
+
+
+@app.route('/cities/<city_id>', methods=["PUT"])
+def update_Cities(city_id):
+    data = request.get_json()
+
+    if not data:
+        return jsonify({'error': "400 Bad Request"}), 400
+
+    city = getCitiesInId(city_id)
+
+    if not city:
+        return jsonify({'error': "404 Not Fund"}), 404
+
+    if not data['name'] or not data['country_code']:
+        return jsonify({'error': "faltan campos en la data"}), 400
+
+    if CodeValidCountry(data['country_code']):
+        return jsonify({'error': "Error de codigo country"}), 400
+
+    if CodeAndCityValid(data['code'], data['name']) and data['name'] != city['name']:
+        return jsonify({'error': "409 Conflisct"}), 409
+
+    city = updateCities(city_id, data)
+
+    if city:
+        return jsonify({"id": city['id'], "name": city['name'],
+                        "country_code": city['country_code'],
+                        "created_at": city['created_at'],
+                        "updated_at": city['updated_at']}), 200
+
+    return jsonify({'error': "error al actualizar"}), 400
+
+
+@app.route('/cities/<city_id>', methods=["DELETE"])
+def delete_Cities(city_id):
+    city = getCitiesInId(city_id)
+
+    if not city_id:
+        return jsonify({'error': 'city not found'}), 404
+
+    if deleteCities(city_id):
+        return jsonify({'message': "City delete OK"}), 204
+
+    return jsonify({'error': "error al deletear"}), 400
