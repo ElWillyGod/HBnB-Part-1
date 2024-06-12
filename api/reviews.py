8eb66de4a6309facba9d61af2847cdaf60e6603b
root <manuelantognazza@gmail.com>
2024-06-11 21:45:47 -0300

    endpoint review

--- api/reviews.py
+++ api/reviews.py
@@ -1,2 +1,130 @@
 #!/usr/bin/python3
-"""reviews endpoint"""
+"""reviews endpoint
+POST /places/{place_id}/reviews: Create a new review for a specified place.
+GET /users/{user_id}/reviews: Retrieve all reviews written by a specific user.
+GET /places/{place_id}/reviews: Retrieve all reviews for a specific place.
+GET /reviews/{review_id}: Retrieve detailed information about a specific review.
+PUT /reviews/{review_id}: Update an existing review.
+DELETE /reviews/{review_id}: Delete a specific review.
+"""
+from flask import Flask, jsonify, request
+
+
+app = Flask(__name__)
+
+
+@app.route('/places/<place_id>/reviews', methods=['POST'])
+def create_Review(place_id):
+    data = request.get_json()
+
+    if not data:
+        return jsonify({'error': 'data no valid'}), 400
+
+    if validCamp(data):
+        return jsonify({'error': 'Missing required fields'}), 400
+
+    if not (1 <= data['rating'] <= 5):
+        return jsonify({'error': 'invalid rating'}), 400
+
+    user = getUser(data['user_id'])
+
+    if not user:
+        return jsonify({'error': 'no user_id'}), 400
+
+    place = getPlace(place_id)
+
+    if not place:
+        return jsonify({'error': 'invalid place_id'}), 400
+
+    if user['id'] == place['host_id']:
+        return jsonify({'error': 'pendejo quiere hacer review de su place'}), 400
+
+    review_id = createReview(place_id, data)
+    return jsonify({'OKa'}), 201
+
+
+@app.route('/users/<user_id>/reviews')
+def get_User_Reviews(user_id):
+    user = getUser(user_id)
+
+    if not user:
+        return jsonify({'error': 'no user'}), 404
+
+    reviews = getUser(user_id)
+
+    return jsonify([{
+        'id': review['id'],
+        'place_id': review['place_id'],
+        'user_id': review['user_id'],
+        'rating': review['rating'],
+        'comment': review['comment'],
+        'created_at': review['created_at'],
+        'updated_at': review['updated_at']
+        } for review in reviews]), 200
+
+
+@app.route('/places/<place_id>/reviews')
+def get_Place_Reviews(place_id):
+    place = getPlace(place_id)
+
+    if not place:
+        return jsonify({'error': 'ya se me gasto el ingles'}), 404
+
+    reviews = getReviewsPlace(place_id)
+    return jsonify([{
+        'id': review['id'],
+        'place_id': review['place_i d'],
+        'user_id': review['user_id'],
+        'rating': review['rating'],
+        'comment': review['comment'],
+        'created_at': review['created_at'],
+        'updated_at': review['updated_at']
+        } for review in reviews]), 200
+
+
+@app.route('/reviews/<review_id>')
+def get_review(review_id):
+    review = getReview(review_id)
+
+    if not review:
+        return jsonify({'error': 'no se encontro la review'}), 404
+
+    return jsonify({
+        'id': review['id'],
+        'place_id': review['place_id'],
+        'user_id': review['user_id'],
+        'rating': review['rating'],
+        'comment': review['comment'],
+        'created_at': review['created_at'],
+        'updated_at': review['updated_at']
+        }), 200
+
+
+@app.route('/reviews/<review_id>', methods=['PUT'])
+def update_review(review_id):
+    review = getReview(review_id)
+
+    if not review:
+        return jsonify({'error': 'not found'}), 404
+
+    data = request.get_json()
+
+    if not data:
+        return jsonify({'error': 'invalid request'}), 400
+
+    if 'rating' in data and not (1 <= data['rating'] <= 5):
+        return jsonify({'error': 'le quiere poner -1 a la review'}), 400
+
+    updateReview(review_id, data)
+    return jsonify({'Oka'}), 200
+
+
+@app.route('/reviews/<review_id>', methods=['DELETE'])
+def delete_review(review_id):
+    review = getReview(review_id)
+
+    if not review:
+        return jsonify({'error': 'review not found'}), 404
+
+    deleteReview(review_id)
+    return jsonify({'OKa delete'}), 204
