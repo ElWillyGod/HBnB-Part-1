d1383192b1db4bef2d5cce289ad3e003a83d6ae8
Alison <8661@holbertonstudents.com>
2024-06-15 10:58:54 -0300

    data manager and persistence updated

--- persistence/DataManager.py
+++ persistence/DataManager.py
@@ -1,23 +1,45 @@
 #!/usr/bin/python3
 
+from abc import ABC, abstractmethod
 """
-data manager
+Data manager
+Handles:
+    Save
+    Get
+    Update
+    Delete
+    Get all
+    Get by property
+    Get countries
 """
+
+
 class IPersistenceManager:
+
+    @abstractmethod
     def save(self, entity):
         pass
-
+    
+    @abstractmethod
     def get(self, entity_id, entity_type):
         pass
 
+    @abstractmethod
     def update(self, entity):
         pass
 
+    @abstractmethod
     def delete(self, entity_id, entity_type):
         pass
 
+    @abstractmethod
     def get_all(self, entity_type):
         pass
 
+    @abstractmethod
     def get_by_property(self, entity_type, property_name, property_value):
+        pass
+
+    @abstractmethod
+    def get_countries(self, code):
         pass
